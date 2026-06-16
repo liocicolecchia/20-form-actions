6cf54602cbd6261c45244058bc5e3465986e8c42
Lio <liocicolecchia@gmail.com>
2026-06-16 23:57:23 +0200

    using action prop instead of onSubmit prop on the form tag

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,10 @@
 export default function Signup() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -17,11 +21,7 @@ export default function Signup() {
 
         <div className="control">
           <label htmlFor="confirm-password">Confirm Password</label>
-          <input
-            id="confirm-password"
-            type="password"
-            name="confirm-password"
-          />
+          <input id="confirm-password" type="password" name="confirm-password" />
         </div>
       </div>
 
@@ -53,22 +53,12 @@ export default function Signup() {
       <fieldset>
         <legend>How did you find us?</legend>
         <div className="control">
-          <input
-            type="checkbox"
-            id="google"
-            name="acquisition"
-            value="google"
-          />
+          <input type="checkbox" id="google" name="acquisition" value="google" />
           <label htmlFor="google">Google</label>
         </div>
 
         <div className="control">
-          <input
-            type="checkbox"
-            id="friend"
-            name="acquisition"
-            value="friend"
-          />
+          <input type="checkbox" id="friend" name="acquisition" value="friend" />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
@@ -80,8 +70,8 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input type="checkbox" id="terms-and-conditions" name="terms" />I agree to the
+          terms and conditions
         </label>
       </div>
 
